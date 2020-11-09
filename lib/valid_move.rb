849f42e977ce1996f3866ed76a051a8e41fdd49d
Michael <wtfdic@hotmail.com>
2020-11-09 21:14:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,6 @@
+# A valid move is both a postion that is on the board and not currently occupied
 def valid_move?(board, index)
+# Check if possition given is valid (empty)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
       return false
@@ -6,7 +8,7 @@ def valid_move?(board, index)
       return true
     end
   end
-
+# Check if possition given is on the board (with 1 - 9 in the Frontend or 0 - 8 in the backend)
   def on_board?(num)
     if num.between?(0, 8) == true
       return true
@@ -14,7 +16,7 @@ def valid_move?(board, index)
       return false
     end
   end
-
+# if position taken is false and on board is true selection is valid
   if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
